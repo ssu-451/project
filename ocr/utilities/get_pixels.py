a46f8ea0c63a96dfaa9df10688fce809728c31a3
Mike <emelyanovma27@yandex.ru>
2018-03-13 16:49:24 +0400

    Changed input for get_pixels for usability

--- ocr/utilities/get_pixels.py
+++ ocr/utilities/get_pixels.py
@@ -7,20 +7,20 @@ from PIL import Image
 import numpy as np
 
 
-def get_pixels(path):
-    """This method takes path to the image as an argument
-    convert it to bi-level format and return two dimensional array of 0,1
+def get_pixels(img):
+    """This method takes image as an argument
+    and convert it to bi-level format
+    and return two dimensional array of 0,1
     where 0 - black 1 - white"""
-    img = Image.open(path)
     bw_img = img.convert('1')
     pix = np.array(bw_img, dtype=int)
     return pix
 
 
-def resize_image(path, size=(15, 15)):
+def resize_image(path, size=(16, 16)):
     """This method takes 2 arguments: path to the image
     and a tuple representing resolution of new image.
-    It returns a new resized image."""
+    It returns a new resized image object."""
     old_img = Image.open(path)
     new_img = old_img.resize(size, Image.LANCZOS)
     return new_img
