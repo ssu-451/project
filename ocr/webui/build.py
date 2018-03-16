c1ec2ea5e11b6187021e98e1db51a4cd32aa54a8
Andy Che <andy.v.che@gmail.com>
2018-03-16 11:03:06 +0400

    Adjusted style

--- ocr/webui/build.py
+++ ocr/webui/build.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
 
-import requests
+"""An executable which is supposed to be called by Jenkins
+after updating the working dir to the github repository
+Launches the web service and reports all errors to Telegram"""
+
 import json
 import subprocess
+import requests
 
 with open('telegram.json') as file:
-    telegram_config = json.load(file)
+    TELEGRAM_CONFIG = json.load(file)
 
 
 def notify(message):
+    """A function which sends the message to Telegram with a header"""
     message = 'Jenkins OCR build:\n{0}'.format(message)
-    requests.get('https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}'.format(
-        telegram_config['token'], telegram_config['chat_id'], message))
+    requests.get(('https://api.telegram.org/bot{0}'
+                  '/sendMessage?chat_id={1}&text={2}').format(
+                      TELEGRAM_CONFIG['token'],
+                      TELEGRAM_CONFIG['chat_id'],
+                      message))
 
 
 def main():
+    """The main function"""
     output = subprocess.Popen('python3 service.py restart -q',
                               shell=True,
-                              stdout=subprocess.PIPE).stdout.read().decode('utf8')
+                              stdout=subprocess.PIPE) \
+        .stdout.read().decode('utf8')
     if output:
         notify(output)
 
