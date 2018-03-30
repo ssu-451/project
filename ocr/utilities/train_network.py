8680e73aea90b85d95cdc97c4a47ac4910d40b51
Andy-ch <andy-ch@mail.ru>
2018-03-31 00:01:52 +0400

    Implemented single symbols recognition with neural network in the web UI

--- ocr/utilities/train_network.py
+++ ocr/utilities/train_network.py
@@ -7,8 +7,10 @@ import sys
 from ocr.basic_nn.tools import training_session
 from ocr.settings import BASE_DIR, NN_LAYERS
 
+REPETITION = 1000
 
-def main():
+
+def main(repetition=REPETITION):
     """
     The main function of the module
     """
@@ -19,9 +21,9 @@ def main():
         assert len(elem['inputs']) == NN_LAYERS[0]
 
     if len(sys.argv) == 2 and sys.argv[1] == 'restart':
-        training_session(train_data, NN_LAYERS, restart=True)
+        training_session(train_data, NN_LAYERS, restart=True, repetition=repetition)
     else:
-        training_session(train_data, NN_LAYERS)
+        training_session(train_data, NN_LAYERS, repetition=repetition)
 
 
 if __name__ == '__main__':
